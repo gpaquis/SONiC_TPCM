514968737a9e76ad7322d3dd7b33465c50c6f52f
Loops <gerald.paquis@sensimedia.org>
2023-10-13 08:55:25 +0100

    Update remote_tpcm.py

--- src/remote_tpcm.py
+++ src/remote_tpcm.py
@@ -134,6 +134,56 @@ def tpcm_install(switch_ip: str, user_name: str, password: str) -> str:
         #print(f'{response}')
         return response.content
 
+def tpcm_upgrade(switch_ip: str, user_name: str, password: str) -> str:
+    """
+        Install TPCM
+        Read from a config file, here the container install from DockerHUB
+        [TPCM1]
+        docker-name = "str",
+        image-source = "str",
+        image-name = "str/str:str",
+        args = "str"
+
+        Image-source must be set with "pull" if container is locate on the dockerhub, otherwise use http/https/usb/sftp/ssh
+    """
+
+    config = configparser.ConfigParser()
+    config.read('remote_tpcm.conf')
+
+    docker_name = config['TPCM1']['docker-name']
+    image_source = config['TPCM1']['image-source']
+    image_name = config['TPCM1']['image-name']
+    tpcm_args = config['TPCM1']['args']
+    skip_data = config['TPCM1']['skip-data-migration']
+
+    request_data = {
+        "openconfig-tpcm:input": {
+           "docker-name": docker_name,
+           "image-source": image_source,
+           "image-name": image_name,
+           "args": tpcm_args,
+           "skip-data-migration": skip_data
+        }
+    }
+
+    #print(json.dumps(request_data))
+    try:
+       response = requests.post(url=f"https://{switch_ip}/restconf/operations/openconfig-tpcm:tpcm-upgrade",
+                                data=json.dumps(request_data),
+                                headers={'Content-Type': 'application/yang-data+json'},
+                                auth=HTTPBasicAuth(f"{user_name}", f"{password}"),
+                                verify=False
+                                )
+       response.raise_for_status()
+
+    except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')
+    except Exception as err:
+        print(f'Other error occurred: {err}')
+    else:
+        #print(f'{response}')
+        return response.content
+
 
 def main():
     parser = argparse.ArgumentParser(description='Remote TPCM tools')
@@ -163,7 +213,11 @@ def main():
        if action == "Remove":
         result = tpcm_remove(switch_ip=switch_ip, user_name=sonic_username, password=sonic_password)
         print(f'{result}')
-
+     
+       if action == "Upgrade":
+        result = tpcm_upgrade(switch_ip=switch_ip, user_name=sonic_username, password=sonic_password)
+        print(f'{result}')
+    
     else:
       print("IP address is not valid\r\nUse tpcm_remote.py -h for Help")
 
